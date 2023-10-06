0d2899f735afcf7f307c26b15721f8fb0ae51c88
test <test@IT-Lab5-V45.NUCFD.edu.local>
2023-10-06 11:02:08 +0500

    Main Calculator Class Added

---
+++ 21F-9501/src/MainCalculatorClass.java
@@ -0,0 +1,11 @@
+
+public class MainCalculatorClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal=new Calculator();
+		cal.add(3.7, 4.5);
+		cal.subtract(8.0, 2.0);
+	}
+
+}
