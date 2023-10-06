9bf239e7228bef4221527370d1dd450dec3b90f5
Muhammad <138708579+umer7475@users.noreply.github.com>
2023-10-06 09:59:14 +0500

    Calculator Class Added

---
+++ 21F-9501/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+	public void add(double a,double b) {
+		System.out.println("Addition:" + (a+b));
+	}
+
+	public void subtract(double a,double b) {
+		System.out.println("Subtraction:" + (a-b));
+	}
+
+	public Calculator() {
+
+	}
+
+}
